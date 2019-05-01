3da01712451b09b9dfec130db6805b4f3a6ac580
syelaseptania <48612589+syelaseptania@users.noreply.github.com>
2019-05-01 21:36:57 +0700

    Create adt_tree.cpp
    membuat awal program

---
+++ adt_tree.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+//pendeklarasian struct sebuah tree awal
+struct Node{
+      int data;
+      Node *kiri;
+      Node *kanan;
+};
