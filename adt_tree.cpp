55c6ab909421e48af4a9f37c0312d00563055468
syelaseptania <48612589+syelaseptania@users.noreply.github.com>
2019-05-01 21:45:43 +0700

    Update adt_tree.cpp
    menambahkan fungsi void node

--- adt_tree.cpp
+++ adt_tree.cpp
@@ -9,3 +9,65 @@ struct Node{
       Node *kiri;
       Node *kanan;
 };
+
+
+
+//fungsi untuk menambahkan node baru
+void tambah(Node **root, int databaru)
+{
+      //jika root masih kosong
+      if((*root) == NULL)
+      {
+            //pembuatan node baru
+            Node *baru;
+            //pengalokasian memori dari node yang telah dibuat
+            baru = new Node;
+            //inisialisasi awal node yang baru dibuat
+            baru->data = databaru;
+            baru->kiri = NULL;
+            baru->kanan = NULL;
+            (*root) = baru;
+            (*root)->kiri = NULL;
+            (*root)->kanan = NULL;
+            cout<<"Data bertambah!"<<endl;
+      }
+     //jika data yang akan dimasukkan lebih kecil daripada elemen root, maka akan diletakkan di node sebelah kiri.
+      else if(databaru<(*root)->data)
+            tambah(&(*root)->kiri, databaru);
+     //jika data yang akan dimasukkan lebih besar daripada elemen root, maka akan diletakkan di node sebelah kanan
+      else if(databaru>(*root)->data)
+            tambah(&(*root)->kanan, databaru);
+     //jika saat dicek data yang akan dimasukkan memiliki nilai yang sama dengan data pada root
+      else if(databaru == (*root)->data)
+            cout<<"Data sudah ada!";
+}
+
+//fungsi yang digunakan untuk mencetak tree secara preOrder
+void preOrder(Node *root)
+{
+      if(root != NULL){
+            cout<<root->data<<endl;
+            preOrder(root->kiri);
+            preOrder(root->kanan);
+      }
+}
+
+//fungsi yang digunakan untuk mencetak tree secara inOrder
+void inOrder(Node *root)
+{
+      if(root != NULL){
+            inOrder(root->kiri);
+            cout<<root->data<<endl;
+            inOrder(root->kanan);
+      }
+}
+
+//fungsi yang digunakan untuk mencetak tree secara postOrder
+void postOrder(Node *root)
+{
+      if(root != NULL){
+            postOrder(root->kiri);
+            postOrder(root->kanan);
+            cout<<root->data<<endl;
+      }
+}
